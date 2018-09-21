5ab6a5f9f2452e9a8a7eaaeb2b4be253dc2d00cb
Robert Rosati <rjrosati@utexas.edu>
2018-09-21 13:58:02 -0500

    now writing state to a file. seperate thread for flask webapp to change statefile. pretty ugly but works

--- pitherm.py
+++ pitherm.py
@@ -1,18 +1,33 @@
 #import Adafruit_DHT
 import time
 import datetime
+import json
 
-DELAY = 1 #s
-THRESHOLD = 0.5 # degC
-HEAT_MODE = False
-COOL_MODE = True
-AC_ON = False
-HEAT_ON = False
+statefile = './state.json'
+state = {
+        'DELAY': 1, #s
+        'THRESHOLD': 0.5, # degC
+        'HEAT_MODE': False,
+        'COOL_MODE': True,
+        'AC_ON': False,
+        'HEAT_ON': False,
+        'TEMP': float('nan'),
+        'TARGET_TEMP': float('nan'),
+        'CURR_PROG': ''
+}
+
+def write_statefile(statefile,state):
+    with open(statefile,'w') as f:
+        f.write(json.dumps(state))
+def read_statefile(statefile):
+    with open(statefile,'r') as f:
+        return json.loads(''.join(f.readlines()))
 
 def F_to_C(t):
     return (float(t)-32)*5/9
 
 def get_desired_temp():
+    global state
     with open('schedule.txt','r') as f:
         lines = f.readlines()
     now = datetime.datetime.now()
@@ -27,32 +42,37 @@ def get_desired_temp():
             temp = float(temp)
         earlyt, latet = map(int,times.split('-'))
         if earlyt < now.hour < latet:
+            state['CURR_PROG'] = prog
             return temp
     raise Exception("Schedule file invalid, some times are unscheduled!")
         
 def measure():
     return float(input('enter measured temp: '))
 
+write_statefile(statefile,state)
 while True:
-    desired_temp = get_desired_temp()
-    print('goal temp: ',desired_temp)
-    temp = measure()
-    if (temp < desired_temp - THRESHOLD):
-        AC_ON = False
-        HEAT_ON = True
-    if (temp > desired_temp + THRESHOLD):
-        AC_ON = True
-        HEAT_ON = False
-    if COOL_MODE:
-        if AC_ON:
+    state = read_statefile(statefile)
+    state['TARGET_TEMP'] = get_desired_temp()
+    print('goal temp: ',state['TARGET_TEMP'])
+    state['TEMP'] = measure()
+    temp = state['TEMP']
+    desired_temp = state['TARGET_TEMP']
+    if (temp < desired_temp - state['THRESHOLD']):
+        state['AC_ON'] = False
+        state['HEAT_ON'] = True
+    if (temp > desired_temp + state['THRESHOLD']):
+        state['AC_ON'] = True
+        state['HEAT_ON'] = False
+    if state['COOL_MODE']:
+        if state['AC_ON']:
             print("turn on AC")
         else:
             print("turn off AC")
-    if HEAT_MODE:
-        if HEAT_ON:
+    if state['HEAT_MODE']:
+        if state['HEAT_ON']:
             print("turn on heat")
         else:
             print("turn off heat")
-
-    time.sleep(DELAY)
+    write_statefile(statefile,state)
+    time.sleep(state['DELAY'])
 
---
+++ webapp.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+statefile = './state.json'
+
+def write_statefile(statefile,state):
+    with open(statefile,'w') as f:
+        f.write(json.dumps(state))
+def read_statefile(statefile):
+    with open(statefile,'r') as f:
+        return json.loads(''.join(f.readlines()))
+
+@app.route('/')
+def index():
+    global statefile
+    template_data = read_statefile(statefile)
+    return render_template('index.html',**template_data)
+
+@app.route('/<action>/<switch>')
+def button_press(action,switch):
+    global state
+    global statefile
+    if action=='cool':
+        if switch=='on':
+            state['COOL_MODE'] = True
+            write_statefile(statefile,state)
+        elif switch=='off':
+            state['COOL_MODE'] = False
+            write_statefile(statefile,state)
+    elif action=='heat':
+        if switch=='on':
+            state['HEAT_MODE'] = True
+            write_statefile(statefile,state)
+        elif switch=='off':
+            state['HEAT_MODE'] = False
+            write_statefile(statefile,state)
+    else:
+        print('unrecognized url!')
+    return render_template('index.html',**state)
+
+
+if __name__ == '__main__':
+    state = read_statefile(statefile)
+    app.run(debug=True,host='127.0.0.1')
